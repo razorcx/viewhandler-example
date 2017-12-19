a68ea8a1d7cc88ea586729e7c2ad29c360bcfdf0
Cory LePage <cory@razorcx.com>
2017-12-19 12:21:15 -0800

    Initial commit

--- ViewHandler/Program.cs
+++ ViewHandler/Program.cs
@@ -1,15 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model.UI;
+using Tekla.Structures.Model;
 
-namespace ViewHandler
+namespace ViewHandlerExample
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			ModelViewEnumerator viewEnum = ViewHandler.GetTemporaryViews();
+
+			while (viewEnum.MoveNext())
+			{
+				var currentView = viewEnum.Current;
+				ViewHandler.HideView(currentView);
+			}
+
+			var picker = new Picker();
+			var connection = picker.PickObject(Picker.PickObjectEnum.PICK_ONE_OBJECT) as Connection;
+
+			if (connection == null) return;
+			
+			var origin = connection.GetCoordinateSystem().Origin;
+
+			var minPoint = new Point
+			{
+				X = origin.X - 200,
+				Y = origin.Y - 200,
+				Z = origin.Z - 300
+			};
+
+			var maxPoint = new Point
+			{
+				X = origin.X + 200,
+				Y = origin.Y + 200,
+				Z = origin.Z + 300
+			};
+			var name = $"Connection: {connection.Name} ({connection.Identifier.ID})";
+			var view = new View
+			{
+				Name = name,
+				ViewCoordinateSystem =
+				{
+					AxisX = new Vector(1, 0, 0),
+					AxisY = new Vector(0, 1, 0),
+					Origin = origin
+				},
+				DisplayCoordinateSystem = new CoordinateSystem()
+				{
+					AxisX = new Vector(1, -.7, 0),
+					AxisY = new Vector(0, 1, 2),
+					Origin = origin
+				},
+				WorkArea =
+				{
+					MinPoint = minPoint,
+					MaxPoint = maxPoint
+				},
+				ViewDepthUp = 500,
+				ViewDepthDown = 500,
+			};
+			view.Insert();
+
+			var views = ViewHandler.GetAllViews();
+			while (views.MoveNext())
+			{
+				var currentView = views.Current;
+				if (currentView.Name == name)
+				{
+					ViewHandler.ShowView(currentView);
+				}
+			}
 		}
 	}
 }
--- ViewHandler/Properties/AssemblyInfo.cs
+++ ViewHandler/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ViewHandler")]
+[assembly: AssemblyTitle("ViewHandlerExample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ViewHandler")]
+[assembly: AssemblyProduct("ViewHandlerExample")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
